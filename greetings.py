d42ed6ebc4ef10f04a2989f340d0a73a1566c062
Safiyya A <146670910+lanternofsea@users.noreply.github.com>
2023-10-27 17:13:17 -0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,3 @@
+
+name = "Safiyya" #assigns the string value "Safiyya" to the variable "name"
+print (f"Hello {name}!") #inserts the assigned string value from the variable "name" into the phrase when printed. "f" or fstring allows the value of the variable to be printed in the phrase.
